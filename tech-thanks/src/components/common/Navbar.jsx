60b15113622dfda9939464f8b90e8f135143f94d
Peter Lewandowski <mapiotr2010@gmail.com>
2022-06-18 16:01:30 -0400

    Navbar changes

--- tech-thanks/src/components/common/Navbar.jsx
+++ tech-thanks/src/components/common/Navbar.jsx
@@ -14,7 +14,9 @@ import { UserContext } from "../context/UserContext";
 
 export default function Navbar() {
   const navigate = useNavigate();
-    const { user, setUser } = useContext(UserContext);
+  
+  const { user, setUser } = useContext(UserContext);
+
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const handleClick = (event) => {
@@ -55,91 +57,26 @@ export default function Navbar() {
 
   return (
     <>
-    <nav
-    style={{
-      display: "flex",
-      flexDirection: "row",
-      justifyContent: "space-between",
-    }}
-  >
-    <div><div>LOGO</div>
-      <img
+      <nav
         style={{
-          width: "25%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
         }}
-        // src={LogoImage}
-        onClick={() => navigate("/")}
-        alt=""
-      />
-    </div>
-    <div>{navButtons(user)}</div>
-  </nav>
+      >
+        <div>
+          <div>LOGO</div>
+          <img
+            style={{
+              width: "25%",
+            }}
+            // src={LogoImage}
+            onClick={() => navigate("/")}
+            alt=""
+          />
+        </div>
+        <div>{navButtons(user)}</div>
+      </nav>
     </>
-    // <Box sx={{ flexGrow: 1 }}>
-    //   <AppBar
-    //     position="fixed"
-    //     style={{ background: "rgba(0, 0, 0, 0.50)", color: "aliceblue" }}
-    //   >
-    //     <Toolbar>
-    //       <IconButton
-    //         size="large"
-    //         edge="start"
-    //         color="inherit"
-    //         aria-label="menu"
-    //         sx={{ mr: 2 }}
-    //       >
-    //         <MenuIcon />
-    //       </IconButton>
-    //       <Typography
-    //         onClick={() => navigate("/")}
-    //         style={{ cursor: "pointer" }}
-    //         variant="h6"
-    //         component="div"
-    //         sx={{ flexGrow: 1 }}
-    //       >
-    //         <img src={LogoImage} />
-    //       </Typography>
-    //       {!user ? (
-    //         <Button onClick={handleClickLogin} color="inherit">
-    //           Login
-    //         </Button>
-    //       ) : (
-    //         <Box sx={{ display: "flex", flexDirection: "row" }}>
-    //           <Typography sx={{ height: "100%", alignSelf: "center" }}>
-    //             {" "}
-    //             Welcome, {user.displayName ? user.displayName : user.name}{" "}
-    //           </Typography>
-    //           <Button
-    //             id="demo-positioned-button"
-    //             aria-controls="demo-positioned-menu"
-    //             aria-haspopup="true"
-    //             aria-expanded={open ? "true" : undefined}
-    //             onClick={handleClick}
-    //           >
-    //             <MenuIcon />
-    //           </Button>
-    //           <Menu
-    //             id="demo-positioned-menu"
-    //             aria-labelledby="demo-positioned-button"
-    //             anchorEl={anchorEl}
-    //             open={open}
-    //             onClose={handleClose}
-    //             anchorOrigin={{
-    //               vertical: "top",
-    //               horizontal: "left",
-    //             }}
-    //             transformOrigin={{
-    //               vertical: "top",
-    //               horizontal: "left",
-    //             }}
-    //           >
-    //             <MenuItem onClick={handleClickLogin}>My account</MenuItem>
-    //             <MenuItem onClick={handleLogout}>Logout</MenuItem>
-    //           </Menu>
-    //         </Box>
-    //       )}
-    //     </Toolbar>
-    //   </AppBar>
-    // </Box>
   );
 }
