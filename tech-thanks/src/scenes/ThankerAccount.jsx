43620142419801720d4bca340ca81f1b8a727880
stolomeo <tolomeosebastian@gmail.com>
2022-06-18 14:35:12 -0400

    Remoed nanoid

--- tech-thanks/src/scenes/ThankerAccount.jsx
+++ tech-thanks/src/scenes/ThankerAccount.jsx
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
 export default function ThankerAccount() {
   return <div>ThankerAccount</div>;
 }
-=======
-import React from 'react';
-
-const ThankerAccount = () => {
-    return (
-        <div>
-            THANKER ACCOUNT
-        </div>
-    );
-}
-
-export default ThankerAccount;
->>>>>>> d08e5908c4dfe28e158fc3754b332bd2c477bddb
