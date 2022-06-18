cb085144ffcb46180b8f92f02caa631e332a3bd9
damian <photoca@hotmail.com>
2022-06-18 15:38:44 -0400

    adding get logged in giver info (your info)

--- tech-thanks/src/components/services/givers.js
+++ tech-thanks/src/components/services/givers.js
@@ -5,3 +5,9 @@ export const getGivers = async () => {
   const response = await client.get("/givers");
   return response.data;
 };
+
+export const getThisGiver = async (address) => {
+  const client = getClient();
+  const giverCall = await client.get("/givers?address="+ address);
+  return giverCall.data;
+}
