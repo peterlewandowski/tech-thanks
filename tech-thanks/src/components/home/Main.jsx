43dae594a0d82e73b18765a86143c6e6852bb1c5
Peter Lewandowski <mapiotr2010@gmail.com>
2022-06-19 00:30:15 -0400

    cleaning up

--- tech-thanks/src/components/home/Main.jsx
+++ tech-thanks/src/components/home/Main.jsx
@@ -1,23 +1,11 @@
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
-import { useNavigate } from "react-router-dom";
 import Leaderboard from "../Leaderboard/Leaderboard";
 
 function Main() {
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    navigate("/SearchResults");
-  };
 
   return (
     <div id="main" style={{ backgroundColor: "#E1D6C6" }}>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit(e);
-        }}
-      >
         <Box
           sx={{
             display: "flex",
@@ -45,7 +33,6 @@ function Main() {
         >
           <Leaderboard />
         </Box>
-      </form>
     </div>
   );
 }
