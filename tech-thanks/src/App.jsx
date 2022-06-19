76b5418574732b9d06f6ffbbb43efeb490989130
stolomeo <tolomeosebastian@gmail.com>
2022-06-19 02:37:44 -0400

    Updated App

--- tech-thanks/src/App.jsx
+++ tech-thanks/src/App.jsx
@@ -2,8 +2,10 @@ import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { blue } from "@mui/material/colors";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Footer from "./components/common/Footer";
+import Navbar from "./components/common/Navbar";
 import { UserContextProvider } from "./components/context/UserContext";
 import Store from "./components/Store";
+import Home from "./scenes/Home";
 import MentorReceive from "./scenes/MentorReceive";
 import ThankerAccount from "./scenes/ThankerAccount";
 import ThankerSend from "./scenes/ThankerSend";
@@ -23,13 +25,13 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Router>
-          {/* <Navbar /> */}
-          <Store />
+          <Navbar />
           <Routes>
-            {/* <Route path="/" element={<Home />} /> */}
+            <Route path="/" element={<Home />} />
             <Route path="/thanker" element={<ThankerAccount />} />
             <Route path="/send" element={<ThankerSend />} />
             <Route path="/thanks/:hash" element={<MentorReceive />} />
+            <Route path="/thanks/:hash" element={<Store />} />
           </Routes>
           <Footer />
         </Router>
