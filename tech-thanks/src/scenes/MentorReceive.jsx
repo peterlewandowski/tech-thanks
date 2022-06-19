e81087d24883cfde559d57f6970a38cb989fd8ea
damian <photoca@hotmail.com>
2022-06-18 21:09:14 -0400

    hash recognized.

--- tech-thanks/src/scenes/MentorReceive.jsx
+++ tech-thanks/src/scenes/MentorReceive.jsx
@@ -1,15 +1,41 @@
 import { useContext } from "react";
 import {UserContext} from "../components/context/UserContext"
 import { Link, useParams } from "react-router-dom";
+import { Input } from '@mui/material'; 
+import { useState } from "react";
+import {oneWayHash,getDomain} from "../components/services/hash"
 
 export default function MentorReceive() {
   const {user,setUser} = useContext(UserContext);
+  const [verifiedEmail,setverifiedEmaill] = useState("")
+  const [typedEmail,setTypedEmail] = useState("")
   let { hash } = useParams();
 
-  return (<>
-  <h2>Mentor Receive</h2>
-  Your Address: {user.address}
-  and your hash is: {hash}
+  const handleEmail = (e) => {
+    setTypedEmail(e.target.value);
+     oneWayHash(e.target.value).then(possibleHash => {
+            if (possibleHash ==hash ) {
+                setverifiedEmaill(e.target.value)
+            }
+    })
+  }
+
+
+
+  return (
+  
+  <>
+    <h2>You've been lucky enough to receive some ThankCoins! Type to cash them out!</h2>
+    {verifiedEmail ? 
+             <div>this is{verifiedEmail} has been verified</div>
+    :
+        <div>
+          We need the email the person that thanked you used:
+             <Input placeholder="Their email" onChange={handleEmail}  />
+         </div>
+     
+    }
+ 
   </>
   )
 }
