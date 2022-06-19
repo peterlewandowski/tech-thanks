6480fc7e92762b82f88cc82c3d3bbe9179c9f560
stolomeo <tolomeosebastian@gmail.com>
2022-06-19 00:27:13 -0400

    Changed app

--- tech-thanks/src/App.jsx
+++ tech-thanks/src/App.jsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-
-import {
-  BrowserRouter as Router, Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // import { createTheme } from "@mui/material";
 // import { ThemeProvider } from "@emotion/react";
 import Home from "./scenes/Home";
@@ -13,8 +8,8 @@ function App() {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/thanker" element={<ThankerAccount />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/thanker" element={<ThankerAccount />} />
       </Routes>
     </Router>
   );
