7209de24ced0b300e685ebfecb78ed81e7de59af
damian <photoca@hotmail.com>
2022-06-19 04:43:31 -0400

    link linky

--- tech-thanks/src/scenes/MentorReceive.jsx
+++ tech-thanks/src/scenes/MentorReceive.jsx
@@ -3,6 +3,9 @@ import { useContext, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../components/context/UserContext";
 import { oneWayHash } from "../components/services/hash";
+import VolunteerActivismIcon from "@mui/icons-material/VolunteerActivism";
+import { Box, Container, Fab, Typography } from "@mui/material";
+import { login, logout } from "../components/services/near/utils";
 
 export default function MentorReceive() {
   const { user, setUser } = useContext(UserContext);
@@ -10,6 +13,11 @@ export default function MentorReceive() {
   const [typedEmail, setTypedEmail] = useState("");
   let { hash } = useParams();
 
+  const handleLogin = (e) => {
+    console.log("handleLogin");
+    login();
+  }
+  
   const handleEmail = (e) => {
     setTypedEmail(e.target.value);
     oneWayHash(e.target.value).then((possibleHash) => {
@@ -26,13 +34,27 @@ export default function MentorReceive() {
         out!
       </h2>
       {verifiedEmail ? (
-        <div>This is {verifiedEmail} has been verified</div>
-      ) : (
-        <div>
-          We need the email the person that thanked you used:
-          <Input placeholder="Their email" onChange={handleEmail} />
-        </div>
-      )}
+        <div>Welcome <b>{verifiedEmail}</b><br /><br />
+         You've been an amazing mentor to someone that wants to thank you 
+                    personally with 5 Tech Thank Stars because of how amazing you are.<br />
+                    <img src="https://media.giphy.com/media/4YY5Yh1qSeCMwQbrnf/giphy.gif" /><br />
+                    <br />
+                    What's next? <br />
+                    Simple! Accept your stars!!! <br />
+                    You'll need your own Near Wallet.<br />
+                    Have one or not just click here to login or create your own:
+                    <Fab variant="extended" onClick={() => handleLogin()}>
+                      <VolunteerActivismIcon sx={{ mr: 1 }} />
+                      Login or Create wallet!
+                    </Fab>
+
+        </div>)
+       : 
+      (  <div>
+          We need the email the person that thanked you used:  
+          <Input placeholder="Your Email" onChange={handleEmail} />
+        </div>)
+      }
     </>
   );
 }
